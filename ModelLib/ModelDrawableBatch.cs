766f9f52f91003eb62d5ddb42fbe1865d5d19485
Domenic Datti <ddatti@hanloninvest.com>
2015-06-27 10:17:55 -0400

    Consolidated using statements in ModelDrawableBatch.cs since I got build errors on my project

--- ModelLib/ModelDrawableBatch.cs
+++ ModelLib/ModelDrawableBatch.cs
@@ -1,17 +1,7 @@
 ﻿using FlatRedBall.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using FlatRedBall;
-using FlatRedBall.Math.Geometry;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
-using System.IO;
-using Microsoft.Xna.Framework.Content.Pipeline.Processors;
-using Microsoft.Xna.Framework.Content.Pipeline;
-
 
 namespace ModelPlugin
 {
